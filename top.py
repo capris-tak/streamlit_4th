8f40b4cd9960aec0ca64afb4963bbeb0142b24d1
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-24 12:41:48 +0900

    Update top.py

--- top.py
+++ top.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import torch
-from transformers import BertJapaneseTokenizer, BertModel, BertForMaskedLM
 
-st.title('BERTによる自然言語処理入門')
-st.write("This is a sample home page in the mutliapp.")
-
-model_name = 'cl-tohoku/bert-base-japanese-whole-word-masking'
-tokenizer = BertJapaneseTokenizer.from_pretrained(model_name)
-
-tokenizer.tokenize('明日はマシンラーニングの勉強をしよう。')
 
 st.image(test.jpg)

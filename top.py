af85a9f0bb686a4912f9a835b1b3362ce8dd2124
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-24 12:45:42 +0900

    Update top.py

--- top.py
+++ top.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import MeCab
 
+mecab = MeCab.Tagger('-Ochasen -d /opt/homebrew/lib/mecab/dic/mecab-ipadic-neologd/')
+sent = "自然言語処理の基本を説明します"
+st.write(mecab.parse(sent))
+
+mecab2 = MeCab.Tagger('-Owakati -d /opt/homebrew/lib/mecab/dic/mecab-ipadic-neologd/')
+st.write(mecab2.parse(sent))
 
 st.image(test.jpg)

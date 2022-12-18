57a50df5f864e3fc18d06bd1b18dc8585c3bb1a5
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 17:53:06 +0900

    Update top.py

--- top.py
+++ top.py
@@ -15,13 +15,13 @@ st.header('Edge Detect')
 
 uploaded_image = st.file_uploader('Choose an image..',type=['png', 'jpg','jpeg','webp'])
 if uploaded_image is not None:
-	with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-		fp = Path(tmp_file.name)
-		fp.write_bytes(uploaded_image.getvalue())
+	#with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+	#	fp = Path(tmp_file.name)
+	#	fp.write_bytes(uploaded_image.getvalue())
 
-		image=Image.open(uploaded_image)
-		img_array = np.array(image)
-		st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
+	image=Image.open(uploaded_image)
+	img_array = np.array(image)
+	st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
 
 
 st.title("Streamlit + OpenCV Sample")

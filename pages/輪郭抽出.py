e0668caafb9c1547f6565626a78af962c09ee870
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 21:59:15 +0900

    Update 輪郭抽出.py

--- pages/輪郭抽出.py
+++ pages/輪郭抽出.py
@@ -39,7 +39,7 @@ if uploaded_image is not None:
 		st.image(contour, caption = '5 contour', use_column_width = True)
 		
 	#save_image = Image.open(contour)
-	btn = st.download_button(label="Download image", data=contour.getvalue(), file_name="edge.png", mime="image/png")
+	#btn = st.download_button(label="Download image", data=contour.getvalue(), file_name="edge.png", mime="image/png")
 	
 	col2, col3, col4 = st.columns(3)
 	with col2:

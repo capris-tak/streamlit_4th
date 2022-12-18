1a69007fff7ddbb3361cb729619ef02f6adc1034
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 17:44:03 +0900

    Update top.py

--- top.py
+++ top.py
@@ -14,13 +14,12 @@ neiborhood8 = np.array([
 st.header('Edge Detect')
 
 uploaded_image = st.file_uploader('Choose an image..',type=['png', 'jpg','jpeg','webp'])
-
+image=Image.open(uploaded_file)
+img_array = np.array(image)
+st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
 
 
 st.title("Streamlit + OpenCV Sample")
-
-
 img = np.zeros((500, 500, 3), np.uint8)
 cv2.rectangle(img, (100, 100), (400, 400), color=(255, 0, 0), thickness=-1)
-
 st.image(img)

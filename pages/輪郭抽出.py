1391b31bb104e13d6181ecf3821592e5d2a5fb82
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-20 00:22:23 +0900

    Update 輪郭抽出.py

--- pages/輪郭抽出.py
+++ pages/輪郭抽出.py
@@ -62,12 +62,12 @@ if uploaded_image is not None:
 		pt1_x = st.slider('left top default:0', 0, width, 0)
 		pt1_y = st.slider('left botom default:0', 0, height, 0)
 	with r_col2:
-		pt2_x = st.slider('right top default:'+str(width), width+1, 0, width)
-		pt2_y = st.slider('right botom default:'+str(height), height+1, 0, height)
+		pt2_x = st.slider('right top default:'+str(width), width, 0, width)
+		pt2_y = st.slider('right botom default:'+str(height), height, 0, height)
 
 	rect = cv2.rectangle(img_array,
-	      pt1=(pt1_x, pt1_y),
-	      pt2=(pt2_x, pt2_y),
+	      pt1=(pt1_x, pt1_y-1),
+	      pt2=(pt2_x, pt2_y-1),
 	      color=(0, 255, 0),
 	      thickness=1,
 	      lineType=cv2.LINE_4,

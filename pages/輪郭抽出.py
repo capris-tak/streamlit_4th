7fc0c6a7437fef6d3cac40fbb33b4165f3fbfff3
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-20 20:36:18 +0900

    Update 輪郭抽出.py

--- pages/輪郭抽出.py
+++ pages/輪郭抽出.py
@@ -85,15 +85,13 @@ import glob
 foulderimgs = glob.glob('pages/井桁弘恵/*')
 #st.write(foulderimgs)
 
-colg1, colg2 = st.columns(2)
+colg1, colg2 = st.beta_columns(2)
 with colg1:
-	foulderimgs = glob.glob('pages/井桁弘恵/*')
-	for foulderimg in foulderimgs:
+	for foulderimg in foulderimgs[::2]:
 		img1 = Image.open(foulderimg)
 		st.image(img1)
 with colg2:
-	foulderimgs = glob.glob('pages/井桁弘恵/*')
-	for foulderimg in foulderimgs:
+	for foulderimg in foulderimgs[1::2]:
 		img2 = Image.open(foulderimg)
 		st.image(img2)
 #st.title("Streamlit + OpenCV Sample")

68318782b6ea1623cdd875435cf12d5fbafb109d
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-21 23:22:51 +0900

    Update 輪郭抽出.py

--- pages/輪郭抽出.py
+++ pages/輪郭抽出.py
@@ -187,16 +187,15 @@ if uploaded_image is not None:
 	for i in range(pm):
 		for j in range(pm):
 			if pm*i+j < len(folder_imgs):
+				plt.axis('off')
 				ax[i, j].xaxis.set_major_locator(plt.NullLocator())
 				ax[i, j].yaxis.set_major_locator(plt.NullLocator())
 				ax[i, j].imshow(d[pm*i+j], cmap="bone")
-				plt.axis('off')
 			else:
+				plt.axis('off')
 				ax[i, j].xaxis.set_major_locator(plt.NullLocator())
 				ax[i, j].yaxis.set_major_locator(plt.NullLocator())
-				plt.axis('off')
 	#plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False, bottom=False, left=False, right=False, top=False)
-
 	st.pyplot(fig)
 	#plt.show()
 	

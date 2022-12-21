c4e6d9aee70477cc3a2f75711f86a6e7923796b3
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-21 21:48:28 +0900

    Update 輪郭抽出.py

--- pages/輪郭抽出.py
+++ pages/輪郭抽出.py
@@ -161,7 +161,7 @@ if uploaded_image is not None:
 		img = cv2.vconcat([img, img_c])
 		
 	img = cv2.vconcat([img, imgbottom])
-	tiled_img = = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	tiled_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 	st.image(tiled_img, caption = 'tile', use_column_width = True)
 	
 	
